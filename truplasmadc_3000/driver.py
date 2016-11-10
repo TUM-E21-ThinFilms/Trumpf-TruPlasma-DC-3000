a084614b61aa854c4a599a37391d535cd2cb6c19
TUM-E21-ThinFilms <alexander.book@frm2.tum.de>
2016-11-10 14:43:09 +0100

    fixing name problem

--- truplasmadc_3000/driver.py
+++ truplasmadc_3000/driver.py
@@ -20,7 +20,7 @@ class TruPlasmaDC3000Driver(Driver):
         
         self.thread = None
         
-        super(TruPlasmaDCSputterDriver, self).__init__(transport, protocol)
+        super(TruPlasmaDC3000Driver, self).__init__(transport, protocol)
 
     def send_message(self, message):
         return self._protocol.query(self._transport, message)
