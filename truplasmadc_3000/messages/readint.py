6d4b741213ad154b04b501bec1fcb691051f2176
TUM-E21-ThinFilms <alexander.book@frm2.tum.de>
2016-11-10 16:01:12 +0100

    Adding license text

--- truplasmadc_3000/messages/readint.py
+++ truplasmadc_3000/messages/readint.py
@@ -1,8 +1,22 @@
+# Copyright (C) 2016, see AUTHORS.md
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from truplasmadc_3000.message import Message, Response, hex_to_float, float_to_hex
 
 class ReadIntMessage(Message):
     def __init__(self):
-        #super(IdentificationMessage, self).__init__(0x6141)
         self.do_allocate(12)
         self.set_command(0x6122)
         
