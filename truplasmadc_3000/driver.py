fbd839847689b64562bf89c1b320c9ddcaeb4114
Alexander Book <alexander.book@frm2.tum.de>
2017-12-07 14:10:29 +0100

    updating driver

--- truplasmadc_3000/driver.py
+++ truplasmadc_3000/driver.py
@@ -27,84 +27,67 @@ from messages.readint import ReadIntMessage
 from messages.setbyte import SetByteMessage
 from messages.readbyte import ReadByteMessage
 
-class TruPlasmaDC3000Driver(Driver):
 
+class TruPlasmaDC3000Driver(Driver):
     def __init__(self, transport, protocol=None):
         if protocol is None:
             protocol = TruPlasmaDC3000Protocol()
-        
+
         self.thread = None
-        
+
         super(TruPlasmaDC3000Driver, self).__init__(transport, protocol)
+
+    def clear(self):
+        self._protocol.clear(self._transport)
 
     def send_message(self, message):
         return self._protocol.query(self._transport, message)
-    
+
     def get_identification(self):
         return self.send_message(IdentificationMessage())
-        
+
     def normal_run(self, voltage, current, power, bits):
         msg = NormalMessage()
-        msg.set_voltage(voltage) # U_max = 800 V
-        msg.set_current(current) # I_max = 1 A
-        msg.set_power(power) # P_max in Watt
-        msg.set_bits(bits) # Power On, PC Control On, Profibus Control On, Display Control On
-        
+        msg.set_voltage(voltage)  # U_max = 800 V
+        msg.set_current(current)  # I_max = 1 A
+        msg.set_power(power)  # P_max in Watt
+        msg.set_bits(bits)  # Power On, PC Control On, Profibus Control On, Display Control On
+
         return self.send_message(msg)
 
-    """
-    def _set_power_single(self, power, current, voltage):
-        msg = NormalMessage()
-        msg.set_voltage(voltage) # U_max = 800 V
-        msg.set_current(current) # I_max = 1 A
-        msg.set_power(power) # P_max in Watt
-        msg.set_bits(0b01000111) # Power On, PC Control On, Profibus Control On, Display Control On
-    
-        return self.send_message(msg)
-    """
-        
     def set_float(self, channel, number):
         msg = SetFloatMessage()
         msg.set_channel(channel)
         msg.set_float(number)
         return self.send_message(msg)
-    
+
     def read_float(self, channel):
         msg = ReadFloatMessage()
         msg.set_channel(channel)
-        
+
         response = self.send_message(msg)
         return response.get_float()
-    
-    def shutdown(self):
-        self.stop_thread()
-        self.set_int(1, 2)
-        
-    def startup(self):
-        self.set_int(1, 1)
-    
+
     def set_int(self, channel, number):
         msg = SetIntMessage()
         msg.set_channel(channel)
         msg.set_int(number)
         return self.send_message(msg)
-    
+
     def read_int(self, channel):
         msg = ReadIntMessage()
         msg.set_channel(channel)
         response = self.send_message(msg)
         return response.get_int()
-    
+
     def set_byte(self, channel, number):
         msg = SetByteMessage()
         msg.set_channel(channel)
         msg.set_byte(number)
         return self.send_message(msg)
-    
+
     def read_byte(self, channel):
         msg = ReadByteMessage()
         msg.set_channel(channel)
         response = self.send_message(msg)
         return response.get_byte()
-    
-    
